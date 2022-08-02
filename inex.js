88122030d4c829e22040db297dd92587e5ffd7db
Felix Cho <felix.cho.kr@gmail.com>
2022-08-02 15:14:56 +0900

    Initial working version

---
+++ inex.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let washswat_tool = {
+    interface: {
+        emergency: require('./lib/interface/emergency'),
+        httpclient: require('./lib/interface/httpclient'),
+        mongodb: require('./lib/interface/mongodb'),
+        redisCache: require('./lib/interface/redisCache'),
+        redisCacheMulti: require('./lib/interface/redisCacheMulti'),
+    },
+    util: {
+        circuitbreaker: require('./lib/util/circuitBreaker'),
+        lap: require('./lib/util/lap'),
+        util2: require('./lib/util/util2'),
+        emergency: require('./lib/util/emergency')
+    }
+};
+
+module.exports = washswat_tool;
